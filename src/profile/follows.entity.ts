44e0eaa3c529780449dc6f4ebc47c68ac3bc1e42
Ying Wang <yingwang@yingqwangVTRWM.local>
2026-08-11 19:35:31 +0800

    test(bc): backward import (expect BC violation)

--- src/profile/follows.entity.ts
+++ src/profile/follows.entity.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity('follows')
 export class FollowsEntity {
+import '../profile/profile.controller';  // bc-test backward
 
   @PrimaryGeneratedColumn()
   id: number;
